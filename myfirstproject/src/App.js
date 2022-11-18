3c01d124681b4f1a099dab8004181ee5f1deb968
james <jamesdanielbetancurt459@gmail.com>
2022-11-18 12:41:49 -0500

    se sube la informacion

---
+++ myfirstproject/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="" id="">
+  <h1>Hello World!</h1>
+  <h2>Bye Group!</h2>
+    </div>);
+  
+}
+
+
+export default App;
